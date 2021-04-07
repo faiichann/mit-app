32fa8fdf8ba6759d5806345292869dbea0c20768
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2021-04-08 01:25:15 +0700

    test1
    ลองเขียนเล่นๆ

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import View from './component/View'
+// import View from './component/View'
 import history from  './history'
+import Somo from './component/somo'
 
 function App() {
   return (
     <div className="App">
         <button onClick={() => history.push('/Dev')}>Developer</button>
-        <View name="faii"/>
+        <Somo name='somoo'/>
+
     </div>
   )
 }
---
+++ src/component/somo.js
@@ -0,0 +1,10 @@
+import React  from 'react'
+
+const Somo = () => {
+    return <>
+    <h1>Hello </h1>
+    </>
+
+
+}
+export default Somo
