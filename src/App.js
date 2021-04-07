a5644e37211f2bbead2ae350a16ef77f0ee4edf9
noojob001 <noojob001@outlook.com>
2021-04-08 01:28:41 +0700

    Test1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import View from './component/View'
+// import View from './component/View'
+import Test1 from './component/Tet'
 import history from  './history'
 
 function App() {
   return (
     <div className="App">
         <button onClick={() => history.push('/Dev')}>Developer</button>
-        <View name="faii"/>
+        <Test1 name="faii"/>
     </div>
   )
 }
---
+++ src/component/Tet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Test1 = () => { 
+    return <>
+        <h1>Hello Test Test Yo Yo</h1>
+    </>
+  }
+  export default Test1
