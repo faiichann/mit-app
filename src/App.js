3adb69d0f642af120e7083fa37a912ca5d6f8fb1
NAMWAN <numwan50@gmail.com>
2021-04-18 21:11:37 +0700

    onChange

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import './App.css';
-import View from './component/View'
-import history from  './history'
+import {useState} from "react";
+
+/*import View from './component/View'
+import history from  './history'*/
+
 
 function App() {
-  return (
-    <div className="App">
-        <button onClick={() => history.push('/Dev')}>Developer</button>
-        <View name="faii"/>
-    </div>
-  )
+  const [textButton, settextButton] = useState({
+    username: 'Text for Button1'
+  });
+  
+  const myChange = (e) => {
+    let nam = e.target.name;
+    let val = e.target.value;
+    settextButton({[nam]: val});
+    }
+      
+
+return (
+        <form>
+        <button>{textButton.username}</button>
+        <input type='text' name='username' 
+        value={textButton.username}
+        onChange={myChange}/>
+      </form>
+        );
 }
 
 export default App;
