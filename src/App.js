630d7f07ded22592fcf4360cc74de901979a5a31
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2021-04-20 21:58:54 +0700

    nothing

--- src/App.js
+++ src/App.js
@@ -2,27 +2,18 @@ import './App.css';
 import View from './component/View'
 import history from  './history'
 import Textfield from './component/Textfield'
-import React ,{useState} from 'react'
+
 
 
 function App() {
-  const [data, setData] = useState({num:''})
-  const divStyle = {
-    width:'{data.number}',
-    height:'{data.number}'
-  }; 
+  
+ 
   return (
     <div className="App">
         <button onClick={() => history.push('/Dev')}>Developer</button>
         <View name="faii"/>
        <Textfield/>
-       <div >
-       <input style={divStyle} type='text'></input>
-       </div>
-       <input type='number' id='nums' value={data.number} onChange={e => setData ({...data, number: e.target.value})}></input>
-       <p >
-         Num : {data.number}
-       </p>
+     
 
     </div>
   )
--- src/component/Textfield.js
+++ src/component/Textfield.js
@@ -1,13 +1,26 @@
-import React  from 'react'
+import React ,{useState} from 'react';
 
-  const Textfield = () => {
+
+function Textfield(){
+const [data, setData] = useState({number:''})
+const divStyle = {
+  width:'{data.number}',
+  height:'{data.number}'
+}; 
+ 
     return <>
         <button>
             Textfield
         </button>
-        
+        <div >
+       <input style={divStyle} type='text'></input>
+       </div>
+       <input type='number' id='nums' value={data.number} onChange={e => setData ({...data, number: e.target.value})}></input>
+       <p >
+         Num : {data.number}
+       </p>
     </>
     
-  }
-
+  
+}
   export default Textfield
