e8e5dd7bc46bcbafd7d14d34a148354c84c7a15f
noojob001 <noojob001@outlook.com>
2021-04-22 22:22:23 +0700

    update FontSize Changer not finish

--- src/component/Fontsize.js
+++ src/component/Fontsize.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
-import { Platform, StyleSheet, View, Text } from 'react-native';
+// import { Platform, StyleSheet, View, Text } from 'react-native';
 
-const options = StyleSheet.create({
-    setFontSizeOne: {
-      fontSize: 15 // Define font size here in Pixels
-    },
-    setFontSizeTwo: {
-      fontSize: 20 // Define font size here in Pixels
-    },
-    setFontSizeThree: {
-      fontSize: 25 // Define font size here in Pixels
-    },
-    setFontSizeFour: {
-      fontSize: 30 // Define font size here in Pixels
-    },
+// const options = StyleSheet.create({
+//     setFontSizeOne: {
+//       fontSize: 15 // Define font size here in Pixels
+//     },
+//     setFontSizeTwo: {
+//       fontSize: 20 // Define font size here in Pixels
+//     },
+//     setFontSizeThree: {
+//       fontSize: 25 // Define font size here in Pixels
+//     },
+//     setFontSizeFour: {
+//       fontSize: 30 // Define font size here in Pixels
+//     },
   
-  });
+//   });
 
-// const options = [
-//     { label: "12", fontSize: "12px"},
-//     { label: "14", fontSize: "14px"},
-//     { label: "16", fontSize: "16px"},
-//     { label: "18", fontSize: "18px"},
-//     { label: "20", fontSize: "14px"}
-// ];
+const options = [
+    { label: "12", fontSize: "12px"},
+    { label: "14", fontSize: "14px"},
+    { label: "16", fontSize: "16px"},
+    { label: "18", fontSize: "18px"},
+    { label: "20", fontSize: "14px"}
+];
 
 class Fontsize extends Component {
     state = {
@@ -46,7 +46,7 @@ class Fontsize extends Component {
             onChange={this.handleChange}
             options={options}
           />
-          <input type="text" style={ options.setFontSizeOne } 
+          <input type="text" style={ options.fontSize } 
           onChange = {this.handleChange.bind(this)}/>
         </form>
         );
