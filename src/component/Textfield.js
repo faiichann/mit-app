343026d107f3699f03eaf86eb87e387041178fa5
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2021-04-23 11:49:29 +0700

    อัพเดตนิดหน่อย

--- src/component/Textfield.js
+++ src/component/Textfield.js
@@ -17,9 +17,15 @@ const divStyle = {
         <Button>
             Textfield
         </Button>
-        <div className="ontainer" >
+        <br></br>
+        <br></br>
+        <div>
        <input style={divStyle} type='text'></input>
        </div>
+       <br></br>
+       <Container>
+       <label for="txtpro">Properties</label>
+       <br></br>
        <label for="txtwidth">Width:</label>
        <input type='number' id='wid' value={data.number} onChange={e => setData ({...data, width: e.target.value})}></input>
        <br>
@@ -33,7 +39,7 @@ const divStyle = {
        <br>
        </br>
        <Button type="submit">Submit</Button>
-       <Button onClick={()=> setData(iniData)}>Delete</Button>
+       <Button onClick={()=> setData(iniData)}>Delete</Button></Container>
         <p >
          Width : {data.width}
          height : {data.height}
