ca5c6d6f727a30c7cf3f28f19f9b47887d982b89
NAMWAN <numwan50@gmail.com>
2021-04-29 21:46:47 +0700

    test for color

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
-import './App.css';
-import {useState} from "react";
+import "./App.css";
+import { useState } from "react";
 
 /*import View from './component/View'
 import history from  './history'*/
 
-
 function App() {
-  const [textButton, settextButton] = useState({
-    username: 'Text for Button1'
+  const [PropButton, setPropButton] = useState({
+    nametext: "Text for Button1",
+    textAlign: "center",
+    backgroudColor: '#e7e7e7'
   });
-  
+
   const myChange = (e) => {
     let nam = e.target.name;
     let val = e.target.value;
-    settextButton({[nam]: val});
-    }
-      
+    setPropButton({ [nam]: val });
+  };
 
-return (
-        <form>
-        <button>{textButton.username}</button>
-        <input type='text' name='username' 
-        value={textButton.username}
-        onChange={myChange}/>
+  const myChangeCSS =(e) => {
+    return<>
+    style={{}}
+    </>
+  };
+
+
+  return (
+    <div>
+      <button className="buttonDefault">{PropButton.nametext}</button>
+      <form>
+        Text: 
+        <input
+          type="text"
+          name="nametext"
+          value={PropButton.nametext}
+          onChange={myChange}
+        />
+        <br/>
+        Text-Align:
+        <input
+        type="text"
+        name="textAlign"
+        value={PropButton.textAlign}
+        onChange={myChange}
+        />
+        <br/>
+        <input
+        type="color"
+        name="backgroudColor"
+        value={PropButton.backgroudColor}
+        onChange={myChange}
+        />
       </form>
-        );
+    </div>
+  );
 }
 
 export default App;
