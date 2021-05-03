2ce6bd8383e5ca53c7ffddb59697ba692c5758dd
Annie745 <supisara.ann@mail.kmutt.ac.th>
2021-05-03 21:26:02 +0700

    Add file from Ann

--- src/component/View.js
+++ src/component/View.js
@@ -2,7 +2,7 @@ import React  from 'react'
 
 const View = (props) => { 
     return <>
-        <h1>Hello this is View component, {props.name}</h1>
+        <h1>{props.name}</h1>
     </>
   }
   export default View
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import BlocklyEditor from './BlocklyEditor';
+import BlocklyToolbox from './BlocklyToolbox';
+import BlocklyToolboxBlock from './BlocklyToolboxBlock';
+import BlocklyToolboxCategory from './BlocklyToolboxCategory';
+import BlocklyWorkspace from './BlocklyWorkspace';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export {
+  BlocklyEditor, BlocklyToolbox, BlocklyToolboxBlock,
+  BlocklyToolboxCategory, BlocklyWorkspace,
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+export default BlocklyEditor;
