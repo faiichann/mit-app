5033e26a8ad9717ec2843442efc5bf80d64fdd2a
Nichkamon Promputta <fainichkamon@gmail.com>
2021-05-04 23:05:19 +0700

    add label component

---
+++ src/component/Label.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { ChromePicker } from "react-color";
+
+class Labelcopy extends React.Component {
+
+    constructor(props) {
+       super(props);
+       this.state = {
+         name: "",
+         background:[ {h: 250,
+          s: 0,
+          l: 0.2,
+          a: 1}],
+          opacityBg : "1",
+          colorPreview : "#333333",
+          size: 12,
+
+      }
+    }
+
+     handleChange(e) {
+        const name = e.target.value;
+        this.setState({name});
+     }
+
+    render() {
+  
+    const handleChangeComplete = data => {
+      if (data.hsl !== this.state.background) {
+        this.setState({ colorPreview :data.hex});
+        this.setState({background:data.hsl});
+        this.setState({opacityBg :data.hsl.a});
+      }
+    };
+      const previewStyle = {
+        background: this.state.colorPreview,
+        opacity: this.state.opacityBg,
+        width: "50px",
+        height: "50px"
+      };
+      const previewfont = {
+        color: this.state.colorPreview,
+      };
+      // const big = { padding: "10px", margin: "2px" };
+      return (
+        <div className="Labelcopy">
+        <div>
+                  <div className="Copy" style={previewStyle} >
+                      <h2 style={{previewfont ,fontSize: this.state.size}} >This is your Text :  {this.state.value}</h2>
+                  </div>
+              </div>
+          <p className="Label">
+            Type your text Here :
+          <input type="text" onChange={this.handleChange.bind(this)}/>
+          </p>
+
+          <ChromePicker color={this.state.background} onChange={handleChangeComplete} /> 
+          <div style={{ fontSize: this.state.size }}>
+
+      {/* <button style={big} onClick={() => this.setState(size + 2)}>
+        A+ Font Size: {this.state.size + 2}
+      </button>
+      <button style={big} onClick={() => this.setState(size - 2)}>
+        A- Font Size: {this.state.size - 2}
+      </button> */}
+    </div>
+
+        </div>
+      );
+    }
+  }
+  export default Labelcopy
