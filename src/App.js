280b4a838cb4da345efe3e34ef9c65b3d8593a76
NAMWAN <numwan50@gmail.com>
2021-05-11 01:38:48 +0700

    test naja

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import "./App.css";
 import Buttontest from './ButtonCom/ButtonComponent'
 import CustomButton from './ButtonCom/CustomButton'
 import ButtonProperties from './ButtonCom/ButtonProperties'
+import { useState } from "react";
 
 /*import View from './component/View'
 import history from  './history'*/
 
 function App() {
+  const [buttonDe, setButtonDe] = useState(
+    {textInput:"textButton01",colorBG:"#e7e7e7",fontSize:"14px",heightBut:"auto",widthBut:"auto"}
+  );
+
+
 
 /*{<button onClick={() => history.push('/Dev')}>Developer</button>}
         {<Test1 name="faii"/>}*/
@@ -14,7 +20,9 @@ function App() {
     <>
     <Buttontest/>
     <CustomButton nametext="testbutton"/>
-    <ButtonProperties/>
+    <ButtonProperties
+    textInput={buttonDe.textInput}
+    colorBG={buttonDe.colorBG} />
     </>
   );
 }
